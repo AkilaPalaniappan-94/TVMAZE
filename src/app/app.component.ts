d374e35dd89625e63b3e807aa5cfc3a431023873
AkilaPalaniappan-94 <akilapl94@gmail.com>
2025-01-14 17:38:54 -0500

    show detail component created, updated app.component.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ShowDetailComponentComponent } from "./show-detail-component/show-detail-component.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ShowDetailComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/show-detail-component/show-detail-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-show-detail-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './show-detail-component.component.html',
+  styleUrl: './show-detail-component.component.css'
+})
+export class ShowDetailComponentComponent {
+
+}
