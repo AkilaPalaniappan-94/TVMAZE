d0f67ec4b9d76154bddf28afd02ee69b5f2c2e38
AkilaPalaniappan-94 <akilapl94@gmail.com>
2025-01-14 19:51:13 -0500

    IshowDetailsData Interface created

---
+++ src/app/ishow-details-data.ts
@@ -0,0 +1,18 @@
+export interface IShowDetailsData {
+    name:string,
+    status:string,
+    language:string,
+    runtime:string,
+    premiered:string,
+    network:{
+        name:string
+    },
+    rating:{
+        average:number
+    },
+    schedule:{
+        time:string
+    }
+
+
+}
