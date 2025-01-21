536b38a20eddc40e993573513c8cb731be656b15
AkilaPalaniappan-94 <akilapl94@gmail.com>
2025-01-20 19:10:41 -0500

    Show Search Component Added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ShowDetailComponentComponent } from "./show-detail-component/show-detail-component.component";
+import { ShowSearchComponent } from './show-search/show-search.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ShowDetailComponentComponent,RouterOutlet],
+  imports: [ShowDetailComponentComponent, RouterOutlet,ShowSearchComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/show-search/show-search.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-show-search',
+  standalone: true,
+  imports: [],
+  templateUrl: './show-search.component.html',
+  styleUrl: './show-search.component.css'
+})
+export class ShowSearchComponent {
+
+}
